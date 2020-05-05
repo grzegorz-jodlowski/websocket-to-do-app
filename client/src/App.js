ad6781fd7b003a1c2027266e490a30f3f8fc20bb
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-05 18:43:18 +0200

    Setup Socket.io client and server sides

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import io from 'socket.io-client';
+let socket = io(`http://localhost:8000`);
+socket.on('updateData', (message) => console.log(message));
+
+
 
 function App() {
   return (
@@ -12,12 +17,12 @@ function App() {
         <h2>Tasks</h2>
 
         <ul className="tasks-section__list" id="tasks-list">
-          <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-          <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+          <li className="task">Shopping <button className="btn btn--red">Remove</button></li>
+          <li className="task">Go out with a dog <button className="btn btn--red">Remove</button></li>
         </ul>
 
         <form id="add-task-form">
-          <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
+          <input className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" />
           <button className="btn" type="submit">Add</button>
         </form>
 
--- server.js
+++ server.js
@@ -2,7 +2,18 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const socket = require('socket.io');
+
+
+const tasks = [];
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+const server = app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  io.to(socket.id).emit('updateData', 'For your eyes only!');
+});
