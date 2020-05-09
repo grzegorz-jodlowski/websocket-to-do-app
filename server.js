4a173b6394eb5c9118aa57b97cdb26eeb67d4342
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-09 18:38:35 +0200

    Prepare server to publish on Heroku

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 
 const express = require('express');
-const app = express();
-const port = 8000;
+const path = require('path');
 const socket = require('socket.io');
+
+const app = express();
+
+const port = 8000;
 
 const tasks = [
   { name: 'Shopping', id: 'dfsadf324s' },
   { name: 'Go out with a dog', id: 'dfs2ad6724s' }
 ];
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
-const server = app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
+app.use(function (req, res, next) {
+  res.status(404).send('Not found...');
+});
+
+const server = app.listen(process.env.PORT || port, () => console.log(`Example app listening at http://localhost:${port}`));
 
 const io = socket(server);
 
